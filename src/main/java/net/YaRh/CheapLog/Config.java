9aec32fd85b6d64efb0900696e4af451cde3d1df
Yet-Another-Rabbit-Hole <yet.another.rabbithole@gmail.com>
2026-09-14 20:46:02 +0200

    Outsourced config into other package

--- src/main/java/net/YaRh/CheapLog/Config.java
+++ src/main/java/net/YaRh/CheapLog/Config.java
@@ -1,8 +1,8 @@
 package net.YaRh.CheapLog;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiConsumer;
+import net.YaRh.ConvConf.Attribute;
+import net.YaRh.ConvConf.Switch;
+
 import java.util.function.Consumer;
 
 /**
@@ -11,103 +11,6 @@ import java.util.function.Consumer;
  * Every output is disabled by default
  */
 public final class Config {
-	
-	/**
-	 * A helper class for config switches for easier access
-	 */
-	public static final class Switch {
-		
-		private static final List<Switch> switches = new ArrayList<>();
-		
-		private static void add(Switch pSwitch) {
-			switches.add(pSwitch);
-		}
-		
-		public static void enableAll() {
-			switches.forEach(Switch::enable);
-		}
-		public static void disableAll() {
-			switches.forEach(Switch::disable);
-		}
-		
-		public static void setAll(boolean pValue) {
-			switches.forEach(swtch -> swtch.set(pValue));
-		}
-		
-		public static void toggleAll() {
-			switches.forEach(Switch::toggle);
-		}
-		
-		public static boolean areAllActive() {
-			return switches.stream().allMatch(Switch::isActive);
-		}
-		
-		private boolean value = false;
-		
-		/**
-		 * The default is always set to {@code false}
-		 */
-		public Switch() {
-			Switch.add(this);
-		}
-		public Switch(boolean pDefault) {
-			this.value = pDefault;
-			Switch.add(this);
-		}
-		
-		public void enable() {
-			this.value = true;
-		}
-		public void disable() {
-			this.value = false;
-		}
-		
-		public void set(boolean pValue) {
-			this.value = pValue;
-		}
-		
-		public boolean toggle() {
-			this.value = !value;
-			return value;
-		}
-		
-		public boolean isActive() {
-			return value;
-		}
-	}
-	
-	public static final class Attribute<T> {
-		
-		private T value = null;
-		private boolean changeable = true;
-		
-		public Attribute(T value, boolean changeable) {
-			this.value = value;
-			this.changeable = changeable;
-		}
-		public Attribute(T value) {
-			this.value = value;
-		}
-		public Attribute() {}
-		
-		public void setFinal() {
-			this.changeable = false;
-		}
-		
-		public T get() {
-			return value;
-		}
-		public boolean isChangeable() {
-			return changeable;
-		}
-		
-		public void set(T value) {
-			if (!changeable)
-				throw new IllegalStateException("You attempted to set an unchangeable attribute");
-				
-			this.value = value;
-		}
-	}
 	
 	public static Switch logging = new Switch(true);
 	public static Switch information = new Switch(false);
